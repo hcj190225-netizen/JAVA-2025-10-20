98e2d81709ab53957c18b828fd9b7b2d05aa73b6
hcj190225 <hcj190225@gmail.com>
2025-10-23 15:39:25 +0900

    java study

--- Java 20251023/src/ex01/비교연산자.java
+++ Java 20251023/src/ex01/비교연산자.java
@@ -2,6 +2,8 @@ package ex01;
 
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class 비교연산자 {
 
 	public static void main(String[] args) {
@@ -53,7 +55,42 @@ public class 비교연산자 {
 		System.out.println(15^12);
 		
 		System.out.println("-----삼향연산자(조건연산자)------");
-		Scanner sc = new Scanner 
+		Scanner sc = new Scanner (System.in);
+		
+		/*
+		 * 나이입력 받아서, 20보다 크면 성년, 아니면 미성년
+		
+		System.out.println("나이 입력>>");
+		int age = sc. nextInt();
+		
+		String state = (age>=20) ? "성년" : "미성년";
+		System.out.println("당신은" + state);
+		*/
+	
+		/*
+		 * 정수 입력 받아서, 2로 나누어서 나머지가 0이면 짝수 아니면 홀수
+		 
+		
+		
+		System.out.println("정수 입력>>");
+		int number = sc . nextInt();
+			
+			String state = (number % 2 == 0) ? "짝수" : "홀수";
+			System.out.println(number + "는" + state + "입니다");	
+		*/
+		
+		
+		/*
+		 * 정수 입력 받아서, 2로 나누어서 나머지가 0이면 짝수 아니면 홀수
+		 */
+		
+		System.out.println("정수 입력>>");
+		int number = sc. nextInt();
+		int state = (number % 3== 0) ? 0 : (number % 3 ==1)? 1 : 2;
+		System.out.println(number +"을 3으로 나눈 나머지"+ state + "입니다.");
+		
+		System.out.pri	
+			
 	}
 
 }
---
+++ Java 20251023/src/ex01/조건문03.java
@@ -0,0 +1,26 @@
+package ex01;
+
+public class 조건문03 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * number >=90 ~A
+		 * number >=80 ~B
+		 * number >=80 ~C
+		 */
+
+		int number =85;
+		
+		if(number >=90) {
+			System.out.println("A학정");
+		}else if(number >=80) {
+			System.out.println("B학정");
+		}else lf 
+			System.out.println("F학정");
+		}else if(number >=95) {
+		System.out.println("C");
+		System.out.println("-----프로그램 종류------");
+	}
+
+}
